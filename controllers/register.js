0500652e0e17a5f5ee0f058490f336caf34be84d
Diane Leigh <leighd2008@gmail.com>
2018-07-13 14:45:37 -0400

    adding port

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
-  if (!email || !name || !pasword) {
+  if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission')
   }
   const hash = bcrypt.hashSync(password);
